8892f434930aa0e3d08ab38902f4b7a619adf5e7
Jonathan Engelbert <jengel.pm.me>
2023-08-05 12:56:20 -0700

    feat(navbar): add navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,117 @@
+'use client'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Link from "next/link";
+
+
+const drawerWidth = 240;
+const navItems = [
+    {
+        index: 0,
+        description: 'Home',
+        route: '/',
+    },
+    {
+        index: 1,
+        description: 'Map',
+        route: '/map',
+    },
+    {
+        index: 2,
+        description: 'About',
+        route: '/about',
+    },
+];
+
+export default function NavBar() {
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{textAlign: 'center'}}>
+            <Typography variant="h6" sx={{my: 2}}>
+                MUI
+            </Typography>
+            <Divider/>
+            <List>
+                {navItems.map(({index, description, route}) => (
+                    <ListItem key={index} disablePadding>
+                        <Link href={route}>
+                            <ListItemButton sx={{textAlign: 'center'}}>
+                                <ListItemText primary={description}/>
+                            </ListItemButton>
+                        </Link>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+
+    return (
+        <Box sx={{display: 'flex'}}>
+            <CssBaseline/>
+            <AppBar component="nav">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{mr: 2, display: {sm: 'none'}}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{flexGrow: 1, display: {xs: 'none', sm: 'block'}}}
+                    >
+                        MUI
+                    </Typography>
+                    <Box sx={{display: {xs: 'none', sm: 'block'}}}>
+                        {navItems.map(({index, description, route}) => (
+                            <Link href={route}>
+                                <Button key={index} sx={{color: '#fff'}}>
+                                    {description}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <Box component="nav">
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: {xs: 'block', sm: 'none'},
+                        '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+        </Box>
+    );
+}
---
+++ src/app/map/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css";
+
+export default function () {
+    return (
+        <main className={styles.main}>
+            <h1>Map</h1>
+        </main>
+    )
+}
