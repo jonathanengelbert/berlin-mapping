8ce8c6773dec5fd632758b264d1b47255d148a6a
Jonathan Engelbert <jengel.pm.me>
2023-08-06 05:56:53 -0700

    feat(map) add map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,14 @@
-import styles from "@/app/page.module.css";
-
-export default function () {
+'use client'
+import dynamic from 'next/dynamic'
+import styles from '../page.module.css'
+export default function MapPage() {
+    const Map = dynamic(
+        () => import('@/app/components/Map'),
+        {ssr: false}
+    )
     return (
-        <main className={styles.main}>
-            <h1>Map</h1>
+        <main className={ styles.map }>
+            <Map/>
         </main>
     )
 }
